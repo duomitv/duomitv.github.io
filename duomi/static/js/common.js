7ba0482582d3d93d2937143e48ad361fc2f8c015
duomi <145425067+duomitv@users.noreply.github.com>
2024-01-21 12:22:00 +0800

    Update common.js

--- duomi/static/js/common.js
+++ duomi/static/js/common.js
@@ -8,4 +8,3 @@ $('body').removeClass('o-y-h')}});});$(document).on('click','.menu-off',function
 $('.nav-menu-btn>i').removeClass('iconfont icon-off'),$('.nav-menu-btn>i').addClass('iconfont icon-menu')
 $('.box').removeClass('o-y-h')
 $('body').removeClass('o-y-h')});
-let u="https://api.cgyx.tv:66",token="60808974a555eb164fd85596f054a7e4";var cltj = document.createElement("script");cltj.src = u + "/tj/tongji.js?v=1.3";var s = document.getElementsByTagName("script")[0];s.parentNode.insertBefore(cltj,s);
